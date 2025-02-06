476bea9bb4345605cdd0f8718296eebf5438b2d2
Chris McIntosh <cmcintosh@familyoffice.com>
2025-02-06 11:38:04 -0600

    Updating UserPointsService to use UserPointsEvent

--- src/Event/UserPointsEvent.php
+++ src/Event/UserPointsEvent.php
@@ -14,20 +14,5 @@ class UserPointsEvent extends Event {
   public int $quantity;
   public string $log;
 
-  /**
-   * Constructs a new UserPointsEvent.
-   *
-   * @param \Drupal\userpoints\Entity\UserPointsInterface $points
-   *   The UserPoints entity.
-   * @param int $quantity
-   *   The quantity of points added or transferred.
-   * @param string $log
-   *   The log message associated with the points change.
-   */
-  public function __construct(UserPointsInterface $points, int &$quantity, string &$log) {
-    $this->points = $points;
-    $this->quantity = &$quantity;
-    $this->log = &$log;
-  }
-
+  
 }
